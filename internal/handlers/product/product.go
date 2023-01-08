eafc53a43d37b8a7f929ec949b8df94324614194
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2023-01-08 18:14:42 +0530

    product handlers

--- internal/handlers/product/product.go
+++ internal/handlers/product/product.go
@@ -1,11 +1,14 @@
 package product
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/Chakravarthy7102/serverless/internal/handlers"
 	"github.com/Chakravarthy7102/serverless/internal/repository/adapter"
 	HttpStatus "github.com/Chakravarthy7102/serverless/utils/http"
+	"github.com/go-chi/chi"
+	"github.com/google/uuid"
 )
 
 type Handler struct {
@@ -21,9 +24,32 @@ func NewHandler(respository adapter.Interface) handlers.Interface {
 	}
 }
 
-func (h *Handler) Get() {}
+func (h *Handler) Get(w http.ResponseWriter, r *http.Request) {
+	if chi.URLParam(r, "ID") != "" {
+		h.GetOne(w, r)
+	} else {
+		h.GetAll()
+	}
+}
 
-func (h *Handler) GetOne() {}
+func (h *Handler) GetOne(w http.ResponseWriter, r *http.Request) {
+	ID, err := uuid.Parse(chi.URLParam(r, "ID"))
+
+	if err != nil {
+		HttpStatus.StatusBadRequest(w, r, errors.New("ID is not valid uuid"))
+	}
+
+	response, err := h.Controller.ListOne(ID)
+
+	if err != nil {
+		HttpStatus.StatusInternalServerError(w, r, err)
+		return
+	}
+
+	HttpStatus.StatusOK(w, r, response)
+}
+
+func (h *Handler) GetAll() {}
 
 func (h *Handler) Post(w http.ResponseWriter, r *http.Request) {
 
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,94 +1,92 @@
-// package routes
+package routes
 
-// import (
-// 	"github.com/go-chi/chi"
-// 	"github.com/go-chi/chi/middleware"
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 
-// 	ServerConfig "github.com/Chakravarthy7102/serverless/config"
-// 	HealthHandler "github.com/Chakravarthy7102/serverless/internal/handlers/health"
-// 	ProductHandler "github.com/Chakravarthy7102/serverless/internal/handlers/product"
-// 	"github.com/Chakravarthy7102/serverless/internal/repository/adapter"
-// )
+	ServerConfig "github.com/Chakravarthy7102/serverless/config"
+	"github.com/Chakravarthy7102/serverless/internal/repository/adapter"
+)
 
-// type Router struct {
-// 	config *Config
-// 	router *chi.Mux
-// }
+type Router struct {
+	config *Config
+	router *chi.Mux
+}
 
-// func NewRouter() *Router {
-// 	return &Router{
-// 		config: NewConfig().SetTimeout(serviceConfig.GetConfig().timeout),
-// 		router: chi.NewRouter(),
-// 	}
-// }
+func NewRouter() *Router {
+	return &Router{
+		config: NewConfig().SetTimeout(ServerConfig.GetConfig().Timeout),
+		router: chi.NewRouter(),
+	}
+}
 
-// func (r *Router) SetRouters(repository adapter.Interface) *chi.Mux {
-// 	r.SetRouterConfigRouters()
-// 	r.RouterHealth(repository)
-// 	r.RouterProduct(repository)
+func (r *Router) SetRouters(repository adapter.Interface) *chi.Mux {
+	r.SetRouterConfigRouters()
+	r.RouterHealth(repository)
+	r.RouterProduct(repository)
 
-// 	return r.router
-// }
+	return r.router
+}
 
-// func (r *Router) SetRouterConfigRouters() {
-// 	r.EnableCORS()
-// 	r.EnableTimeout()
-// 	r.EnableLogger()
-// 	r.EnableRecovery()
-// 	r.EnableRealIP()
-// 	r.EnableRequestId()
-// }
+func (r *Router) SetRouterConfigRouters() {
+	r.EnableCORS()
+	r.EnableTimeout()
+	r.EnableLogger()
+	r.EnableRecovery()
+	r.EnableRealIP()
+	r.EnableRequestId()
+}
 
-// func (r *Router) RouterHealth(repository adapter.Interface) {
-// 	handler := HealthHandler.newHandler(repository)
-// 	r.router.Route("/health",func(route chi.Route){
-// 		route.Post("/",handler.Post)
-// 		route.Get("/",handler.Get)
-// 		route.Put("/",handler.Put)
-// 		route.Delete("/",handler.Delete)
-// 		route.Options("/",handler.Options)
+func (r *Router) RouterHealth(repository adapter.Interface) {
+	handler := HealthHandler.newHandler(repository)
+	r.router.Route("/health", func(route chi.Route) {
+		route.Post("/", handler.Post)
+		route.Get("/", handler.Get)
+		route.Put("/", handler.Put)
+		route.Delete("/", handler.Delete)
+		route.Options("/", handler.Options)
 
-// 	})
-// }
+	})
+}
 
-// func (r *Router) RouterProduct(repository adapter.Interface) {
-// 	handler := ProductHandler.newHandler(repository)
+func (r *Router) RouterProduct(repository adapter.Interface) {
+	handler := ProductHandler.newHandler(repository)
 
-// 	r.router.Route("/product",func(route chi.Route){
-// 		route.Post("/",handler.Post)
-// 		route.Get("/",handler.Get)
-// 		route.Put("/{ID}",handler.Put)
-// 		route.Delete("/{ID}",handler.Delete)
-// 		route.Options("/",handler.Options)
-// 	})
-// }
+	r.router.Route("/product", func(route chi.Route) {
+		route.Post("/", handler.Post)
+		route.Get("/", handler.Get)
+		route.Put("/{ID}", handler.Put)
+		route.Delete("/{ID}", handler.Delete)
+		route.Options("/", handler.Options)
+	})
+}
 
-// func (r *Router) EnableTimeout() *Router {
-// 	r.router.Use(middleware.Timeout(r.config.GetTimeout()))
-// 	return r
-// }
+func (r *Router) EnableTimeout() *Router {
+	r.router.Use(middleware.Timeout(r.config.GetTimeout()))
+	return r
+}
 
-// func (r *Router) EnableCORS()*Router {
-// 	r.router.Use(r.config.Cors)
-// 	return r
-// }
+func (r *Router) EnableCORS() *Router {
+	r.router.Use(r.config.Cors)
+	return r
+}
 
-// func (r *Router) EnableRecovery()*Router {
-// 	r.router.Use(middleware.Recoverer)
-// 	return r
-// }
+func (r *Router) EnableRecovery() *Router {
+	r.router.Use(middleware.Recoverer)
+	return r
+}
 
-// func (r *Router) EnableRequestId()*Router {
-// 	r.router.Use(middleware.RequestId)
-// 	return r
-// }
+func (r *Router) EnableRequestId() *Router {
+	r.router.Use(middleware.RequestId)
+	return r
+}
 
-// func (r *Router)  EnableRealIP()*Router {
-// 	r.router.Use(middleware.RealIP)
-// 	return r
-// }
+func (r *Router) EnableRealIP() *Router {
+	r.router.Use(middleware.RealIP)
+	return r
+}
 
-// func (r *Router)  EnableLogger( ) *Router {
-// 	r.router.Use(middleware.Logger)
-// 	return r
-// }
+func (r *Router) EnableLogger() *Router {
+	r.router.Use(middleware.Logger)
+	return r
+}
